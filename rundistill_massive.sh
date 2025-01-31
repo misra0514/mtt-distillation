5a8190c1c334ff17ca6167ca9495229a6d7e8e0c
misra0514 <1242383961@qq.com>
2025-01-31 17:46:23 -0500

    pruning method 2 for time exp

--- rundistill_massive.sh
+++ rundistill_massive.sh
@@ -1,8 +1,9 @@
 
-for i in 3 4 5 6 7 8 9 10 11 12 13 14 15
+for i in 10 11 12
 do
+# j=(($i-1))
 WANDB_SILENT=true python distill.py --dataset=CIFAR10 --pix_init=real --ipc=1  \
     --syn_steps=$i --max_experts=1 --expert_epochs=1 --max_start_epoch=1 --Iteration=0 \
-    --zca --lr_img=1000 --lr_lr=1e-05 --lr_teacher=0.01 \
+    --detachNum=$(($i-10)) --zca --lr_img=1000 --lr_lr=1e-05 --lr_teacher=0.01 \
     --buffer_path=./buffer --data_path=./dataset
 done
